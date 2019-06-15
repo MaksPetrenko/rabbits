af0336594198bea922a3b576be4f7c83341eb395
Maks <petrenkomaks405@gmail.com>
2019-06-15 18:27:09 +0300

    input and output

---
+++ src/main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class main {
+    public static void main(String []args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("введіть ім'я 1 кролика");
+        String string = s.nextLine();
+        System.out.println("ім'я кролика 1 "+string);
+        System.out.println("введіть ім'я 2 кролика");
+        String pups =s.nextLine();
+        System.out.println("ім'я кролик 2 "+pups);
+        System.out.println("ведіть ім'я 3 кролика");
+        String guf = s.nextLine();
+        System.out.println("ім'я кролика 3 "+guf);
+        System.out.println("введіть настрій 1 кролика");
+        String puf = s.nextLine();
+        System.out.println("настрій кролика 1 "+puf);
+        System.out.println("введіть настрій 2 кролика");
+        String buf = s.nextLine();
+        System.out.println("настрій кролика 2 "+buf);
+        System.out.println("ведіть настрій 3 кролика");
+        String fuf = s.nextLine();
+        System.out.println("астрій кролика 3 "+fuf);
+        System.out.println("------------------------------------");
+        System.out.println("|\tBobik\t|\tSimba\t|\tBig Bob\t|");
+        System.out.println("------------------------------------");
+        System.out.println("|\tPlayful\t|\tSad\t\t|\tBad\t\t|");
+        System.out.println("------------------------------------");
+        System.out.println("ввести що сказав 1 кролик");
+        String bon =s.nextLine();
+        System.out.println("кролик 1 сказав "+bon);
+        System.out.println("ввести що сказав 2 кролик");
+        String bib = s.nextLine();
+        System.out.println("кролик 2 сказав "+bib);
+        System.out.println("ввести що сказав 3 кролик");
+        String bis = s.nextLine();
+        System.out.println("кролик 3 сказав "+bis);
+        System.out.println("--------------------------------------------");
+        System.out.println("|\tliet's play\t|\tI feel bad\t|\tI'm ill\t|");
+        System.out.println("--------------------------------------------");
+
+        System.out.println("--------------------------------------------");
+        System.out.println("|\tBobik\t\t|\tSimba\t|\tBig Bob\t\t|");
+        System.out.println("--------------------------------------------");
+        System.out.println("|\tPlayfull\t|\tSad\t\t|\tBad\t\t|");
+        System.out.println("--------------------------------------------");
+        System.out.println("|\tlet's play\t|\tI feel bad\t|\tI'm ill\t|");
+        System.out.println("--------------------------------------------");
+    }
+}
